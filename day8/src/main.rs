7ed61e5454a00ae165e15b8c593b10975b827314
Timur <timur.makarchuk@gmail.com>
2018-12-09 17:39:58 +0500

    Day8 part 2 is probably down. Can't check it as I'm on a plane (can't complain about it though)

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -16,7 +16,6 @@ impl Node {
             .next()
             .ok_or("Invalid format. Can't read header")
             .unwrap();
-        println!("Loading {} children", child_num);
         let children = (0..child_num)
             .map(|_| Box::new(Node::from_iter(iter)))
             .collect();
@@ -33,6 +32,25 @@ impl Node {
         self.metadata.iter().map(|meta| *meta as u128).sum::<u128>()
             + self.children.iter().map(|ch| ch.meta_sum()).sum::<u128>()
     }
+
+    fn node_value(&self) -> u128 {
+        if self.children.len() == 0 {
+            self.metadata.iter().map(|meta| *meta as u128).sum::<u128>()
+        } else {
+            self.metadata
+                .iter()
+                .map(|id| match id {
+                    0 => 0,
+                    _ => {
+                        let i = id - 1;
+                        match self.children.get(i as usize) {
+                            None => 0,
+                            Some(child) => child.node_value(),
+                        }
+                    }
+                }).sum()
+        }
+    }
 }
 
 fn main() {
@@ -40,4 +58,5 @@ fn main() {
     io::stdin().read_to_string(&mut input).unwrap();
     let nodes = Node::from_iter(&mut input.split(" ").map(|el| el.parse().unwrap()));
     println!("Metadata sum is {}", nodes.meta_sum());
+    println!("Node value is  {}", nodes.node_value());
 }
